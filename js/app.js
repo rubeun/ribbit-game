f0818a973da521e0b1abb6d6b91be2a289a42702
Rubeun Tan <github@rubeun.com>
2019-04-03 10:18:51 -0700

    feat: add player class with render

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
+// Constants
+const PLAYER_START_X = 200; 
+const PLAYER_START_Y = 400;
+
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+
+    // Enemy coordinates
+    this.x = 0;
+    this.y = 0;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -24,12 +32,31 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+
+    // Player coordinates
+    this.x = PLAYER_START_X;
+    this.y = PLAYER_START_Y;
+
+    this.sprite = 'images/char-boy.png';
+}
+
+Player.prototype.update = function() {
+
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+let allEnemies = [];
+allEnemies.push(new Enemy);
 
+let player = new Player();
 
 
 // This listens for key presses and sends the keys to your
