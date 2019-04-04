48e16d44349546ef948cd30f9d1b4694876fa40a
Rubeun Tan <github@rubeun.com>
2019-04-03 20:05:47 -0700

    feat: enemy collision detection working

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 // CONSTANTS
 const PLAYER_START_X = 200; 
 const PLAYER_START_Y = 440;
-const PLAYER_MOVE_INCREMENTS = 10;
+const PLAYER_MOVE_INCREMENTS = 20;
 const NUM_ENEMIES = 4;
 const ENEMY_TOP_BOUNDARY = 50;
 const ENEMY_BOTTOM_BOUNDARY = 300;
-const ENEMY_MIN_SPEED = 10;
-const ENEMY_MAX_SPEED = 80;
+const ENEMY_MIN_SPEED = 50;
+const ENEMY_MAX_SPEED = 150;
 
 // UTILITIES
 // Create a random number in between 2 numbers
@@ -25,12 +25,18 @@ var Enemy = function(initialX, initialY, enemySpeed) {
     this.x = initialX;
     this.y = initialY;
 
+    // Keeps track of players coords for collision detection
+    this.playerX = 0;
+    this.playerY = 0;
+
     // Enemy speed
     this.speed = enemySpeed;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+
+
 };
 
 // Update the enemy's position, required method for game
@@ -43,11 +49,18 @@ Enemy.prototype.update = function(dt) {
     if (this.x < 500) {
         this.x += (this.speed * dt);
     } else {
-        this.x = 0;
+        this.x = -100;
     }
 
     // Collision handling
-
+    let playerX = player.playerXCoord();
+    let playerY = player.playerYCoord();
+    //console.log("Enemy coords: " + this.x + ", " + this.y);
+    //console.log("Player coords: " + playerX + ", " + playerY);
+    if ((this.x > (playerX - 20)) && (this.x < (playerX + 20)) && ((this.y > (playerY - 20)) && (this.y < (playerY + 20)))) {
+        console.log("Player Dies - Player coords: " + playerX + "," + playerY + " Enemy coords: " + this.x + "," + this.y);
+        player.playerDies();
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -55,40 +68,43 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
+// PLAYER
 // This class requires an update(), render() and
 // a handleInput() method.
 var Player = function() {
+    // Score
+    this.wins = 0;
+    this.deaths = 0;
 
     // Player coordinates
     this.x = PLAYER_START_X;
     this.y = PLAYER_START_Y;
 
     this.sprite = 'images/char-boy.png';
+
+    this.playerXCoord = function() {
+        return this.x;
+    }
+    this.playerYCoord = function() {
+        return this.y;
+    }
+
+    this.playerDies = function() {
+        this.x = PLAYER_START_X;
+        this.y = PLAYER_START_Y;
+    }
 }
 
 // Update player's location
 Player.prototype.update = function(action) {
-    switch(action) {
-        case 'moveUp' :  
-            this.y -= PLAYER_MOVE_INCREMENTS;
-            break;
-        case 'moveDown' :  
-            this.y += PLAYER_MOVE_INCREMENTS;
-            break;
-        case 'moveLeft' :  
-            this.x -= PLAYER_MOVE_INCREMENTS;
-            break;
-        case 'moveRight' :  
-            this.x += PLAYER_MOVE_INCREMENTS;
-            break;
 
-        case 'reset' :
-            this.x = PLAYER_START_X;
-            this.y = PLAYER_START_Y; 
-            break;
-        default:
-            //console.log("Invalid action:", action);
+    // Player reaches water. Toggle win.
+    if (this.y < 0) {
+        // Player Wins!
+        console.log("Player Wins!");
+        // Reset to start
+        this.x = PLAYER_START_X;
+        this.y = PLAYER_START_Y; 
     }
 }
 
@@ -102,16 +118,16 @@ Player.prototype.handleInput = function(direction) {
 
     switch(direction) {
         case 'up' :
-            player.update('moveUp');
+            this.y -= PLAYER_MOVE_INCREMENTS;
             break;
         case 'down' :
-            player.update('moveDown');
+            this.y += PLAYER_MOVE_INCREMENTS;
             break;
         case 'left' :
-            player.update('moveLeft');
+            this.x -= PLAYER_MOVE_INCREMENTS;
             break;
         case 'right' :
-            player.update('moveRight');
+            this.x += PLAYER_MOVE_INCREMENTS;
             break;
         default:
             console.log("Input direction invalid: ", direction);
@@ -120,21 +136,29 @@ Player.prototype.handleInput = function(direction) {
 }
 
 
-// Now instantiate your objects.
+// INIT
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-let allEnemies = [];
-let enemySpacing = Math.floor((ENEMY_BOTTOM_BOUNDARY - ENEMY_TOP_BOUNDARY) / (NUM_ENEMIES - 1));
-let enemyStartY = ENEMY_TOP_BOUNDARY;
-for (let i = 0; i < NUM_ENEMIES; i++) {
-    let enemySpeed = randomNum(ENEMY_MIN_SPEED, ENEMY_MAX_SPEED);
-    allEnemies.push(new Enemy(0, enemyStartY, enemySpeed));
-    enemyStartY += enemySpacing;
-}
 
 let player = new Player();
 
+let allEnemies = [];
+let enemySpacing = 0;
+let enemyStartY = ENEMY_TOP_BOUNDARY;
 
+if (NUM_ENEMIES > 1) {
+    enemySpacing = Math.floor((ENEMY_BOTTOM_BOUNDARY - ENEMY_TOP_BOUNDARY) / (NUM_ENEMIES - 1));
+} 
+
+for (let i = 0; i < NUM_ENEMIES; i++) {
+    let enemySpeed = randomNum(ENEMY_MIN_SPEED, ENEMY_MAX_SPEED);
+    let newEnemy = new Enemy(-100, enemyStartY, enemySpeed);
+    allEnemies.push(newEnemy);
+    enemyStartY += enemySpacing;
+}
+
+
+// EVENT LISTENERS
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
